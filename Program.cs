00aae93282a57cdc0aa4080331f75c128738dc02
Bruno Costa <br9uno@gmail.com>
2024-06-18 19:27:00 -0300

    [complete] Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Criando celular Nokia:");
+
+Smartphone n1 = new Nokia(numero: "61982101717", modelo: "Galaxy", imei: "ASD568D", memoria: 128);
+
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo($"Google Maps");
+System.Console.WriteLine("");
+
+System.Console.WriteLine("Criando celular Iphone:");
+
+Smartphone i1 = new Iphone(numero: "61982101717", modelo: "Galaxy", imei: "ASD568D", memoria: 128);
+
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Google Maps");
